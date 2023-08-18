b36a8891454de6a0f82dac202edfb5a7e8502439
Tkumar02 <ta_265@yahoo.co.uk>
2023-08-18 17:08:58 +0100

    adding things to firebase programmatically

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-categories',
@@ -7,11 +8,27 @@ import { Component } from '@angular/core';
 })
 export class CategoriesComponent {
   categoryData: any = {}
+  subCat: any = {}
   
-  constructor() {}
+  constructor( private afs: AngularFirestore ) {}
 
   onSubmit(form: any){
     this.categoryData.category = form.value.category
+    this.categoryData.active = false
+
+    this.subCat.field = 'subCAT1'
+
     console.log(this.categoryData)
+
+    this.afs.collection('categories').add(this.categoryData).then(docRef=> {
+      console.log(docRef.id);
+      this.afs.collection('categories').doc(docRef.id).collection('subCategories').add(this.subCat).then(docRef1=>{
+        console.log(docRef1);
+
+        this.afs.doc(`categories/${docRef.id}/subCategories/${docRef1.id}`).collection('subsubCategories').add(this.subCat)
+      })
+    })
+    .catch(err => {console.log(err)})
   }
+
 }
